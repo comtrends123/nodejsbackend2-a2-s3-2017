83143d14b2ad3d8435100f5091980f813ac4e9a0
Mai Pham Quang Huy <maipham.quanghuy@gmail.com>
2017-12-29 13:22:26 +0700

    Update shoppingcart.js

--- shoppingcart.js
+++ shoppingcart.js
@@ -3,6 +3,12 @@ var Schema = mongoose.Schema;
 var ShoppingCarts = new Schema({
     id: Number,
     name: String,
+    price: Number,
+    description: String,
+    brand: String,
+    producer: String,
+    inventory: Number,
+    imageurl: String,
 });
 
 module.exports = mongoose.model('ShoppingCarts', ShoppingCarts);
